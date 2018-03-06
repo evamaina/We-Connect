43152a6e60e2cb9eadeab586f4588a570a9e05e7
Eva <evajohnson714@gmail.com>
2018-03-06 12:37:08 +0300

    [Fix_bug #155735130] remove status code bug

--- src/api.py
+++ src/api.py
@@ -9,6 +9,10 @@ app.config['SECRET_KEY'] = 'thisissecret'
 users = []
 business_list = []
 
+"""
+Register route and function 
+register user: a method that adds a user to the system
+"""
 
 @app.route('/api/auth/register', methods=['POST'])
 def register_user():
@@ -19,13 +23,18 @@ def register_user():
     if (len(users) > 0):
         for x, k in enumerate(users):
             if k['username'] == username:
-                return jsonify({'Message': "Username already exists : " + username}), 200
+                return jsonify({'Message': "Username already exists : " + username}), 409
             else:
                 users.append(user)
                 return jsonify({'Message': 'User registered successfully'}), 201
     else:
         users.append(user)
         return jsonify({'Message': 'User registered successfully'}), 201
+
+        """
+    Login route and function 
+    login: a method that allws user to login
+    """
 
 
 @app.route('/api/auth/login', methods=['POST'])
@@ -36,9 +45,35 @@ def login_user():
 
     for x, k in enumerate(users):
         if k['username'] == username and k['password'] == password:
-            return jsonify({'Message': "User :" + username + " logged in Successifuly"}), 201
+            return jsonify({'Message': "User :" + username + " logged in Successfuly"}), 201
     else:
-        return jsonify({'Message': 'User does not exist or wrong username/password.'}), 404
+        return jsonify({'Message': 'User does not exist or wrong username/password.'}), 401
+
+
+    """
+    Business route and function 
+    register business: a method that adds a busness to the list
+    """
+
+
+@app.route('/api/business', methods=['POST'])
+def register_business():
+    request_data = request.get_json()
+    business = {'business_name': request_data['business_name'], 'country': request_data['country'],
+                'id': len(business_list) + 1}
+    business_name = request_data['business_name']
+
+
+    if (len(business_list) > 0):
+        for x, k in enumerate(business_list):
+            if k['business_name'] == business_name:
+                return jsonify({'Message': "business already exists, use a different name from : " + business_name}), 200
+            else:
+                business_list.append(business)
+                return jsonify({'Message': 'Business registered successfully'}), 201
+    else:
+        business_list.append(business)
+        return jsonify({'Message': 'business registered successfully'}), 201
 
 
 
