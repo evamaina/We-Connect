100435d3082820f94143b6215cc17a8f16e035d6
Eva <evajohnson714@gmail.com>
2018-03-07 09:14:52 +0300

    [finishes #155694944] Create method for resetting password

--- tests/test_api.py
+++ tests/test_api.py
@@ -51,7 +51,37 @@ class UserAuthClass(unittest.TestCase):
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn("registered successfully", response_msg["Message"])
 
+    def test_user_can_update_business(self):
+        """
+        Tests new user can add a business to the system.
+        """
+        response = self.app.put("/api/businesses/<businessId>",
+                                    data=json.dumps(dict(businessId="testbusinessid",
+                                                    country="testcountry")),
+                                 content_type="application/json")
+        self.assertEqual(response.status_code, 404)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Business does not exist", response_msg["Message"])
+
+    def test_user_can_reset_password(self):
+        """
+        Tests new user can add a business to the system.
+        """
+        response = self.app.post("/api/auth/reset-password",
+                                    data=json.dumps(dict(username="testusername",
+                                                    old_password="testpassword",
+                                                    new_password="testnew")),
+                                 content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("password-reset done Successifuly", response_msg["Message"])
 
 
+    
+
+
+
+
+    
 
 
