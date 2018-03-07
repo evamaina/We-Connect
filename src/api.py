956c22271eae8eccfda33ae8f2571df415d86009
Eva <evajohnson714@gmail.com>
2018-03-07 09:11:33 +0300

    [feature #155694944] Create test to test password

--- src/api.py
+++ src/api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
-import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
 
 app = Flask(__name__)
 
@@ -74,6 +74,42 @@ def register_business():
     else:
         business_list.append(business)
         return jsonify({'Message': 'business registered successfully'}), 201
+
+
+
+
+@app.route('/api/businesses/<businessId>', methods=['PUT'])
+def update_business(businessId):
+    request_data = request.get_json()
+    business_Id=businessId
+    if (len(business_list) > 0):
+        for k in range(0,len(business_list)):
+            if business_list[k]['id'] == business_Id:
+                business_list[k]['business_name']=request_data['business_name']
+                business_list[k]['country']=request_data['country']
+                return jsonify({'Message': "business updated" }), 200
+
+        return jsonify({'Message': 'Business does not exist'}), 404
+    else:
+        return jsonify({'Message': 'No business record in the system'}), 404
+
+
+
+@app.route('/api/auth/reset-password', methods=['POST'])
+def reset_password():
+    request_data = request.get_json()
+    username = request_data['username']
+    old_password = request_data['old_password']
+    new_password = request_data['new_password']
+
+    for x, k in enumerate(users):
+        if k['username'] == username and k['password'] == old_password:
+            k['password']=new_password
+
+            return jsonify({'Message': "User :" + username + " password-reset done Successifuly"}), 200
+    else:
+        return jsonify({'Message': 'Enter the new password and try again.'}), 401
+
 
 
 
