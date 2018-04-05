11b5fa5c0a77c33a2a468f166320396b5a8a2326
Eva <evajohnson714@gmail.com>
2018-04-05 14:55:29 +0300

    debug file

--- models/users.py
+++ models/users.py
@@ -1,20 +1,20 @@
-from flask import Flask, jsonify, request
+# from flask import Flask, jsonify, request
 
 
 class User(object):
 
-  def __init__(self):
-    self.users=[]
-    
-  def create_user(self, id, username, email, password):
-    new_user = {
-           'id': id,
-           'username': username,
-           'email': email,
-           'password': password,
-           'login_status':False
-           }
+    def __init__(self):
+        self.users = []
 
-    self.users.append(new_user)
+    def create_user(self, id, username, email, password):
+        new_user = {
+            'id': id,
+            'username': username,
+            'email': email,
+            'password': password,
+            'login_status': False
+        }
 
-    return self.users
+        self.users.append(new_user)
+
+        return self.users
