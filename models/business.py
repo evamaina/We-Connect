1c48e8fbd90a9275d622fae6c74ebbfa30605ff6
Eva <evajohnson714@gmail.com>
2018-04-05 14:50:23 +0300

    debug file

--- models/business.py
+++ models/business.py
@@ -1,19 +1,20 @@
-from flask import Flask, jsonify, request
+# from flask import Flask, jsonify, request
+
 
 class Business(object):
 
     def __init__(self):
         self.businesses = []
 
-    def add_business(self, id, name, category, country,userid):
+    def add_business(self, id, name, category, country, userid):
         new_business = {
-           'id': id,
-           'name': name,
-           'category': category,
-           'country': country,
-           'userid':userid
-           }
+            'id': id,
+            'name': name,
+            'category': category,
+            'country': country,
+            'userid': userid
+        }
 
-        self.users.append(new_user)
+        self.users.append(new_business)
 
         return self.businesses
